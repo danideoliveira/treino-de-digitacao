4f539028f3ebf80ecf199fdd617b46a2c9f1c301
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-09-23 21:01:34 -0300

    Inserindo novos elementos dark

--- public/modules/darkMode.js
+++ public/modules/darkMode.js
@@ -1,25 +1,14 @@
 import { darkModeElements } from '../constants/darkModeElements.js'
 
-export class DarkMode {
-    constructor(arr) {
-        const currentTheme = localStorage.getItem('theme');
-        if (currentTheme === 'dark') this.setDarkMode(arr);
-
-        const btnDarkMode = document.querySelector('.btn-dark-mode');
-        btnDarkMode.addEventListener('click', () => {
-            this.setDarkMode(arr);
-            this.toggleLocalStorage();
-        });
-    }
-
+const darkModePrototypes = {
     setDarkMode(arr) {
-        for(let currentElement of arr) {
+        for (let currentElement of arr) {
             const { element } = currentElement;
             const el = document.querySelector(element);
-            if(!el) continue;        
+            if (!el) continue;
             el.classList.toggle('dark');
         }
-    }
+    },
 
     toggleLocalStorage() {
         const body = document.querySelector('body');
@@ -27,42 +16,18 @@ export class DarkMode {
             ? localStorage.setItem('theme', 'light')
             : localStorage.setItem('theme', 'dark')
     }
+};
+
+export function DarkMode(arr) {
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') this.setDarkMode(arr);
+
+    const btnDarkMode = document.querySelector('.btn-dark-mode');
+    btnDarkMode.addEventListener('click', () => {
+        this.setDarkMode(arr);
+        this.toggleLocalStorage();
+    });
 }
 
+DarkMode.prototype = darkModePrototypes;
 const setDarkMode = new DarkMode(darkModeElements);
-
-function mouseOver() {
-    const element = document.querySelectorAll('.div-form > form > button');
-    const body = document.querySelector('body')
-    
-    element.forEach(currentElement => {
-        const img = currentElement.children[0];
-        const p = currentElement.children[1];
-
-        currentElement.addEventListener('mouseover', () => {
-            if(!body.classList.contains('dark')) return;
-
-            if(body.classList.contains('dark')) {
-                img.classList.add('mouse-over');
-                p.classList.add('mouse-over');
-            } else {
-                img.classList.remove('mouse-over');
-                p.classList.remove('mouse-over');
-            }
-        });
-
-        currentElement.addEventListener('mouseout', () => { 
-            if(!body.classList.contains('dark')) return;
-
-            if(body.classList.contains('dark')) {
-                img.classList.remove('mouse-over');
-                p.classList.remove('mouse-over');
-            } else {
-                img.classList.add('mouse-over');
-                p.classList.add('mouse-over');
-            }
-        });
-    })
-}
-
-mouseOver();
