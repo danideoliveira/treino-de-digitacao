27e3498730dac4b696688eeb24926c567cf4f998
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-10-11 15:49:16 -0300

    Renomeando para index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.urlencoded({extended: true}));
+
+// Utilizando arquivos estáticos através de caminho absoluto
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Definindo views e engine EJS
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Iniciando rotas definidas
+app.use(routes);
+
+// Porta definida para o servidor
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
